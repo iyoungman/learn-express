c2d7e687ebfdc78b831167e1435df4af1b6023a6
iyoungman <hsl9546@gmail.com>
2019-05-13 22:36:14 +0900

    Express 라우터 사용 // Controller 분리하여 라우팅과 비즈니스 로직 분리

--- app.js
+++ app.js
@@ -1,43 +1,31 @@
-require('dotenv').config();
+require('dotenv').config()
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import createError from 'http-errors'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+import indexRouter from './routes/index'
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express()
 
-var app = express();
-
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use('/', indexRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
-});
+app.use((req, res, next) => {
+    next(createError(404))
+})
 
 // error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+    res.locals.message = err.message
+    res.locals.error = process.env.NODE_ENV === 'development' ? err : {}
+    return res.status(err.status || 500)
+        .json(res.locals.error)
+})
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
-
-module.exports = app;
+// bin/www 를 그대로 사용하기 위해서 예외적으로 commonJs 문법을 적용
+module.exports = app
